f1d770aa5d3b89529250af1820bd0df5994813db
IK <wndlsrnr1@gmail.com>
2021-10-05 11:39:06 +0900

    퀴즈 관련 인터페이스 등 틀 잡기.

--- Quiz/src/quizio/QuizManager.java
+++ Quiz/src/quizio/QuizManager.java
@@ -2,12 +2,7 @@ package quizio;
 
 public class QuizManager {
 	
-	private static QuizManager qManager = new QuizManager();
-	
-	
-	public static QuizManager getManager() {
-		return qManager;
-	}
+
 	
 	
 }
