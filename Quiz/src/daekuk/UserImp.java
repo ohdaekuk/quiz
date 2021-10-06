8487f5fab3564e9ee89db7c232c240e93e58fe3a
user <user@121.131.36.216>
2021-10-06 12:40:21 +0900

    지금까지 진도

--- Quiz/src/daekuk/UserImp.java
+++ Quiz/src/daekuk/UserImp.java
@@ -38,7 +38,7 @@ public class UserImp implements UserDao{
 		String sql = "insert into user value(0, ?, ?, ?, ?, ?)";
 		
 		try(Connection conn = QuizConn.getConn();
-				PreparedStatement pst = conn.prepareStatement(sql)){
+			PreparedStatement pst = conn.prepareStatement(sql)){
 			
 			pst.setString(1, user.getUser_Id());
 			pst.setString(2, user.getUser_Password());
@@ -226,10 +226,88 @@ public class UserImp implements UserDao{
 	
 
 	@Override
-	public int update() {
+	public int update() throws ClassNotFoundException, SQLException {
 		
-		return 0;
+		String sql = "update user set";
 		
+		System.out.println("변경 할 user_num을 입력하세요");
+		String user_num = scan.nextLine();
+		
+		System.out.println("user_id를 변경하시겠습니까? 변경 값을 입력하세요. (변경하지 않으면 엔터)");
+		String user_id = scan.nextLine();
+		
+		if (!user_id.equals("")) { 
+			sql += " user_id = ?";
+		}
+		
+		System.out.println("user_password를 변경하시겠습니까? 변경 값을 입력하세요. (변경하지 않으면 엔터)");
+		String user_password = scan.nextLine();
+		
+		if (!user_password.equals("")) { 
+			sql += " user_password = ?";
+		}
+		
+		System.out.println("나이를 변경하시겠습니까? 변경 값을 입력하세요. (변경하지 않으면 엔터)");
+		String age = scan.nextLine();
+		
+		if (!age.equals("")) { 
+			sql += " age = ?";
+		}
+		
+		System.out.println("성별을 변경하시겠습니까? 변경 값을 입력하세요. (변경하지 않으면 엔터)");
+		String gender = scan.nextLine();
+		
+		if (!gender.equals("")) { 
+			sql += " gender = ?";
+		}
+		
+		System.out.println("별명을 변경하시겠습니까? 변경 값을 입력하세요. (변경하지 않으면 엔터)");
+		String user_nickName = scan.nextLine();
+		
+		if (!user_nickName.equals("")) { 
+			sql += " user_nickName = ?";
+		}
+		
+		// sql에 where 절 추가
+		
+		sql += " where user_num = ?";
+		
+		try(Connection conn = QuizConn.getConn();
+				PreparedStatement pst = conn.prepareStatement(sql)){
+			
+			int cnt = 1;
+			
+			
+			if (!user_id.equals("")) {
+				pst.setString(++cnt, user_id);
+			}
+			
+			if (!user_password.equals("")) {
+				pst.setString(++cnt, user_password);
+			}
+			
+			if (!age.equals("")) {
+				pst.setInt(++cnt, Integer.parseInt(age));
+			}
+			
+			if (!gender.equals("")) {
+				pst.setString(++cnt, gender);
+			}
+			
+			if (!user_nickName.equals("")) {
+				pst.setString(++cnt, user_nickName);
+			}
+			
+			pst.setInt(1, Integer.parseInt(user_num));
+			
+			int rows = pst.executeUpdate();
+			System.out.println(rows + "개 수정 끝!");
+			
+			
+			
+			return 0;
+			
+		}
 	}
-	
 }
+
