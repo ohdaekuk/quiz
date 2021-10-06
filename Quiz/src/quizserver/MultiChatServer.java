f81f94cc56e9ed9ee8fbeee3f400ab56513c8357
IK <wndlsrnr1@gmail.com>
2021-10-06 18:34:57 +0900

    유저 서

--- Quiz/src/quizserver/MultiChatServer.java
+++ Quiz/src/quizserver/MultiChatServer.java
@@ -10,16 +10,23 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import daekuk.RankingImp;
+import daekuk.UserImp;
 import quizio.QuizUtil;
 
 
 public class MultiChatServer {
 	private List<MultiChatServer.User> userList;
 	private ServerSocket serverSocket;
-	private Map<String, String> quizList = QuizUtil.getUtil().quizToServer();
+	
+	//QuizUtill: 퀴즈 관련 입출력 있는 메서드 모음 클래스.
+	private QuizUtil util = QuizUtil.getUtil();
+	private Map<String, String> quizList = util.quizToServer();
+	
 	private LocalDateTime startTime;
 	private LocalDateTime finishTime;
 	
@@ -55,73 +62,181 @@ public class MultiChatServer {
 			this.socket = socket;
 			bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 		}
+		
+		String thisUserId = "";
+		String thisUserPassword = "";
+		String thisUserNickName = "";
+		int thisUserNumber = 0;
+		int thisUserScore = 0;
+		int gameCount = 0;
+		int userCount = 0;
+		//여기 있는 변수 위에 있는 클래스랑 합치기.  
+		
+		
+		//내부 클래스를 써서 데이터 베이스와 연결된 User객체와 이 객체 둘다 쓰는 것이 좋은지 아니면, 
+		//내부 클래스를 없애고 데이터 베이스와 연결된 User객체 하나만 쓰는 것이 좋은지 
+		
+		
 		//문제를 불러오는 메서드 
 		//로그를 텍스트에 쓰는 메서드 
 		@Override
 		public void run() {
+			Iterator<String> keys = quizList.keySet().iterator();
+			//넥스트 한번 돌때마다 게임 카운트 숫자 올라감.
+			String question = keys.next();
+			gameCount++;
+			//키 값으로 문제 정답 받기.
+			String answer = quizList.get(question);
+			
+			daekuk.User thisUser = null;
+			
 			BufferedReader br = null;
+			
 			try {
-				System.out.println("---서버 스레드 실행---");
+				System.out.println("---퀴즈 프로그램 실행---");
 				br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				//bw.write("아이디를 입력하세요");
-				//bw.flush
-				//this.id = br.reaLine();
-				//
-				//bw.write("비밀번호를 입력하세요");
-				//bw.flush
-				//this.password = br.readLine();
-				//
-				//데이터 베이스 메소드 이용해서 입력받은 비밀번호와 일치한지 확인하기
-				//if 일치한다면, System.out.println("접속 성공");
-				//else (일치하지 않는다면) 계속해서 아이디와 비밀번호 입력받기.
 				
-				//회원가입을 여기에서 할 것인지? 아니면 다른 메인 메소드를 가진 클래스에서 할 것인지?  
 				
-				bw.write("사용할 닉네임을 입력하세요. \n");
-				bw.flush();
 				
-				this.name = br.readLine();
-				for(User user : userList) {
-					if(this != user) {
-						user.bw.write(name+"님이 접속하였습니다.");
-						user.bw.newLine();
-						user.bw.flush();
+				//로그인 로직 시작.
+				while(true) {
+					bw.write("아이디를 입력하세요");
+					bw.flush();
+					this.id = br.readLine();
+					
+					bw.write("비밀번호를 입력하세요");
+					bw.flush();
+					this.password = br.readLine();
+					
+					List<daekuk.User> dbUserList = UserImp.getInstance().userFindAll();
+					//여기 나중에 한 폴더로 옮겨서 보기.
+					
+					
+
+					
+					for(daekuk.User u : dbUserList) {
+						thisUserId = u.getUser_Id();
+						//반복문 탈출하기 위해 최종 기록된 userId값을 저장.
+						thisUserPassword = u.getUser_Password();
+						//반복문 탈출하기 위해 최종 기록된 userPassword를 저장.
+						
+						if(thisUserId.equals(id)&&thisUserPassword.equals(password)) {
+							bw.write("로그인되었습니다.");
+							bw.flush();
+							thisUserNumber = u.getUser_num();
+							break;
+						}
+					}
+					
+					if(thisUserId.equals(id)&&thisUserPassword.equals(password)) {
+						break;
+					}else {
+						bw.write("아이디 또는 비밀번호가 옳바르지 않습니다.");
+						bw.flush();
 					}
 				}
-				//사용자가 입력했을 때 메세지를 보내기
+				userCount++;
+				//접속 성공하면 유저 카운트 올라감.
+				
+				
+				
+				
+				
+				for(User user : userList) {
+						user.bw.write(thisUserNickName+"님이 접속하였습니다.");
+						user.bw.newLine();
+						user.bw.flush();
+				}
+				
+				
+				
 				String msg = null;
+				
+				
+				
 				while(true) {
-					//연결되어 있는 사람의 메세지를 읽고
-					msg = br.readLine();
-					if(msg == null) {
-						throw new Exception();
+					
+					if(userCount == 3) {
+						for(User user : userList) {
+							LocalDateTime startTime = LocalDateTime.now();
+							user.bw.write("[게임을 시작하겠습니다.]");
+							user.bw.write(question);						
+							}
 					}
-					//모든 사람에게 뿌려준다.
+					
+					msg = br.readLine();
+					//사용자가 보낸 메세지 읽고 
+					util.saveLog(msg);
+					//바로 저장한다.
 					for(User user :userList) {
 						if(this != user) {
-							user.bw.write("["+name+"] : " + msg);
+							user.bw.write("["+thisUserNickName+"] : " + msg);
 							user.bw.newLine();
 							user.bw.flush();
 						}
 					}
-					//받은 메세지가 지금 낸 문제와 동일 하다면
+					//모든 사람에게 메세지 보내기.
+
 					
-					//정답 처리를 한다.
+					if(msg == null) {
+						throw new Exception();
+					}
 					
-					//정답처리{ Map의 key = value인지 확인, userList중 정답을 맞춘 유저 에게 점수를 주고 User의 score 에 score += 점수 형태로 저장, 
-					//정답입니다 메세지 보내기 }
 					
-					//다음 문제로 넘어간다.
+					//정답을 맞출때 로직 
+					if(msg.equals(quizList.get(msg))) {
+						//이터레이터로 키의 다음 값 문제 받기 
+						question = keys.next();
+						//맵에서 퀴즈 문제 받아서 다음 정답 세팅.
+						quizList.get(question);
+						//맞춘 사람 점수 올리기.
+						thisUserScore += 10;
+						//게임 숫자 카운트.
+						gameCount++;
+						//문제 모두에게 보내기.
+						for(User user : userList) {
+							user.bw.write("["+thisUserNickName+"] : " + "이 정답을 맞추었습니다.");
+							user.bw.newLine();
+							user.bw.flush();
+							
+							
+							user.bw.write("다음문제는.. ");
+							user.bw.flush();
+							user.bw.write(question);
+							
+						}
+					}
 					
-					//다음 문제를 고르는 메서드 로직 구현.
+										
 					
-					//마무리에서 점수를 데이터 베이스에 등록, 게임 시작 시작시간 마무리 시간 등 기록하기.  
+					//게임 종료 시점
+					if(gameCount == 4) {
+						//게임 종료시 게임 시작 시간 기록하기 위해 변수 선언.
+						LocalDateTime endTime = LocalDateTime.now();
+						for(User user : userList) {
+							user.bw.write("게임이 종료되었습니다.");
+							user.bw.newLine();
+							user.bw.flush();
+						}
+						break;
+						//일단 게임은 끝나는 걸로 해놓음. 
+						//종료 되었으므로 게임 진행하며 저장된 사항 모두 저장
+						//게임은 계속 진행하도록 할 것인지 아니면 찬반 투표를 할 것인지? 
+					}
+					
+					
+					
+					
+					  
 				}
 			}catch(Exception e) {
 				userList.remove(this);
 				try {
+					userCount--;
+					//접속 종료하면 유저 수 감소함. 
+					
 					for(User user : userList) {
-						user.bw.write(name + "님이 방을 나갔습니다.");
+						user.bw.write(thisUserNickName + "님이 방을 나갔습니다.");
 						user.bw.newLine();
 						user.bw.flush();
 					}
